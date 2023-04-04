5f1f277b4653ed30f6169e9e1f8ffa555942b7e9
Selvakumar <skajay1622@gmail.com>
2023-04-04 16:26:05 +0530

    AppWrite constants are written

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,6 @@
+import 'package:appwrite/appwrite.dart';
+
+Client client = Client();
+// client.setEndpoint('https://8080-appwrite-integrationfor-tq1jyi10dis.ws-us93.gitpod.io/v1')
+//     .setProject('642ab54cd3bbf633a8be')
+//     .setSelfSigned(status: true); // For self signed certificates, only use for development
