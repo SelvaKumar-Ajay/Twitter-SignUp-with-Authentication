e91ebeb56055fdbe77e78968610636ffe6273332
Selvakumar <skajay1622@gmail.com>
2023-04-04 17:23:16 +0530

    set themecolors and constants to reduce boiler plate codes

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -1,6 +1,11 @@
-import 'package:appwrite/appwrite.dart';
-
-Client client = Client();
+// import 'package:appwrite/appwrite.dart';
+// Client client = Client();
 // client.setEndpoint('https://8080-appwrite-integrationfor-tq1jyi10dis.ws-us93.gitpod.io/v1')
 //     .setProject('642ab54cd3bbf633a8be')
 //     .setSelfSigned(status: true); // For self signed certificates, only use for development
+class AppWriteConstants {
+  static const String dataBaseId = '642bff36bae00dab7be4';
+  static const String projectId = '642ab54cd3bbf633a8be';
+  static const String endPoint =
+      'https://8080-appwrite-integrationfor-tq1jyi10dis.ws-us93.gitpod.io/console/organization-642ab54c2dc404188506';
+}
